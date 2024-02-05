0eb8cb634ca90d0738ef2e9f49e1fa717de96957
rizilmaharjan <rizilmaharjan@gmail.com>
2024-02-05 23:00:35 +0545

    Feature: Add user to db with dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('Helloworld')
+
+        ]);
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
